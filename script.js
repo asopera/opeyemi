3be6e8b05eee7a22262257310458276608bad41a
asopera <105983660+asopera@users.noreply.github.com>
2022-05-21 12:52:51 -0700

    commit

--- script.js
+++ script.js
@@ -1,7 +1,67 @@
+//prototypes for the 6 movies
+function redemption() {
+    this.redemption = 'redemption';
+    return this;
+  }
+  redemption.prototype.test = function(){return redemption;}
+  
+  function africa tech food() {
+    this.tech food = 'africa tech food';
+    return this;
+  }
+  Bar.prototype.test2 = function(){return africa tech roots;}
+  
+  function Cool() {
+    Foo.call(this);
+    Bar.call(this);
+  
+    return this;
+  }
+  Bar.prototype.test2 = function(){return complicated project;}
+  
+  function Cool() {
+    Foo.call(this);
+    Bar.call(this);
+  
+    return this;
+    Bar.prototype.test2 = function(){return africa tech roots;}
+  
+    function Cool() {
+      Foo.call(this);
+      Bar.call(this);
+    
+      return this;
+      Bar.prototype.test2 = function(){return the invisible man;}
+  
+      function Cool() {
+        Foo.call(this);
+        Bar.call(this);
+      
+        return this;
+        Bar.prototype.test2 = function(){the complicated Project;}
+  
+        function Cool() {
+          Foo.call(this);
+          Bar.call(this);
+        
+          return this;
+          Bar.prototype.test2 = function(){return simply javascript;}
+  
+          function Cool() {
+            Foo.call(this);
+            Bar.call(this);
+          
+            return this;
 
-let js = "amazing";
-console.log("jonas");
-
-
-let firstName="opera";
-console.log(firstName);
+  
+  var combine = Object.create(Foo.prototype);
+  $.extend(combine, Object.create(Bar.prototype));
+  
+  Cool.prototype = Object.create(combine);
+  Cool.prototype.constructor = Cool;
+  
+  var cool = new Cool();
+  
+  console.log(cool.redemption,africa tech roots,the invisble LockManager,the complicated Project,simply javascript,the new web());
+  
+  
